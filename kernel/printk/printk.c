29df8853063d3d01aec39abf3f91ac99f7800aa5
Chris Down <chris@chrisdown.name>
2022-05-17 19:15:16 +0000

    Use LOGLEVEL_INVALID as default for console_loglevel

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -62,7 +62,7 @@
 #include "internal.h"
 
 int console_printk[4] = {
-	CONSOLE_LOGLEVEL_DEFAULT,	/* console_loglevel */
+	LOGLEVEL_INVALID,		/* console_loglevel (forced) */
 	MESSAGE_LOGLEVEL_DEFAULT,	/* default_message_loglevel */
 	CONSOLE_LOGLEVEL_MIN,		/* minimum_console_loglevel */
 	CONSOLE_LOGLEVEL_DEFAULT,	/* default_console_loglevel */
