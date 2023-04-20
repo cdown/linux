bb5ebaef228379190c04d9c40f6ffa1b11739e5c
Chris Down <chris@chrisdown.name>
2023-04-20 12:08:04 +0000

    printk: console: Support console-specific loglevels
    Consoles can have vastly different latencies and throughputs. For
    example, writing a message to the serial console can take on the order
    of tens of milliseconds to get the UART to successfully write a message.
    While this might be fine for a single, one-off message, this can cause
    significant application-level stalls in situations where the kernel
    writes large amounts of information to the console.
    
    This means that while you might want to send at least INFO level
    messages to (for example) netconsole, which is relatively fast, you may
    only want to send at least WARN level messages to the serial console.
    Such an implementation would permit debugging using the serial console
    in cases that netconsole doesn't receive messages during particularly
    bad system issues, while still keeping the noise low enough to avoid
    inducing latency in userspace applications. This patch adds such an
    interface, extending the existing console loglevel controls to allow
    each console to have its own loglevel.
    
    One can't just disable the serial console, because one may actually need
    it in situations where the machine is in a bad enough state that nothing
    is received on netconsole. One also can't just bump the loglevel at
    runtime after the issue, because usually the machine is already so
    wedged by this point that it isn't responsive to such requests.
    
    In terms of technical implementation, this patch embeds a device pointer
    in the console struct, and registers each console using it so we can
    expose attributes in sysfs. We currently expose the following
    attributes:
    
        % ls -l /sys/class/console/ttyS/
        total 0
        lrwxrwxrwx 1 root root    0 Jul 20 17:37 subsystem -> ../../../../class/console/
        -r--r--r-- 1 root root 4096 Jul 20 17:38 effective_loglevel
        -r--r--r-- 1 root root 4096 Jul 20 17:38 effective_loglevel_source
        -r--r--r-- 1 root root 4096 Jul 20 17:38 enabled
        -rw-r--r-- 1 root root 4096 Jul 20 17:38 loglevel
        -rw-r--r-- 1 root root 4096 Jul 20 17:37 uevent
    
    The lifecycle of this classdev looks like this on registration:
    
        register_console(con)/printk_late_init()
          console_register_device(con)
            device_initialize(con->classdev) # refcount++
            device_add(con->classdev)        # refcount++
    
    At stable state, the refcount is two.
    
    Console unregistration looks like this:
    
        [con->classdev refcount drops to 0]
          console_classdev_release(con->classdev)
            kfree(con->classdev)
    
        unregister_console(con)
          device_unregister(con->classdev)
            device_del(con->classdev) # refcount--
              device_remove_class_symlinks()
                kernfs_remove_by_name_ns()
                  kernfs_drain()
                    kernfs_drain_open_files() # wait for close()
            put_device(con->classdev) # refcount--
    
    We also deprecate the kernel.printk sysctl as it doesn't know about
    per-console loglevels, and is generally pretty confusing.
    
    For information on the precedence and application of the new controls,
    see Documentation/ABI/testing/sysfs-class-console and
    Documentation/admin-guide/per-console-loglevel.rst.
    
    Signed-off-by: Chris Down <chris@chrisdown.name>

--- kernel/printk/sysctl.c
+++ kernel/printk/sysctl.c
@@ -7,9 +7,13 @@
 #include <linux/printk.h>
 #include <linux/capability.h>
 #include <linux/ratelimit.h>
+#include <linux/console.h>
 #include "internal.h"
 
 static const int ten_thousand = 10000;
+
+static int min_loglevel = LOGLEVEL_EMERG;
+static int max_loglevel = CONSOLE_LOGLEVEL_MOTORMOUTH;
 
 static int proc_dointvec_minmax_sysadmin(struct ctl_table *table, int write,
 				void *buffer, size_t *lenp, loff_t *ppos)
@@ -20,13 +24,48 @@ static int proc_dointvec_minmax_sysadmin(struct ctl_table *table, int write,
 	return proc_dointvec_minmax(table, write, buffer, lenp, ppos);
 }
 
+static int printk_sysctl_deprecated(struct ctl_table *table, int write,
+				    void *buffer, size_t *lenp, loff_t *ppos)
+{
+	int res = proc_dointvec(table, write, buffer, lenp, ppos);
+
+	if (write)
+		pr_warn_once(
+			"printk: The kernel.printk sysctl is deprecated. Consider using kernel.console_loglevel or kernel.default_message_loglevel instead.\n");
+
+	return res;
+}
+
+static int printk_console_loglevel(struct ctl_table *table, int write,
+				   void *buffer, size_t *lenp, loff_t *ppos)
+{
+	struct ctl_table ltable = *table;
+	int ret, level;
+
+	if (!write)
+		return proc_dointvec(table, write, buffer, lenp, ppos);
+
+	ltable.data = &level;
+
+	ret = proc_dointvec(&ltable, write, buffer, lenp, ppos);
+	if (ret)
+		return ret;
+
+	if (level != -1 && level != clamp_loglevel(level))
+		return -ERANGE;
+
+	console_loglevel = level;
+
+	return 0;
+}
+
 static struct ctl_table printk_sysctls[] = {
 	{
 		.procname	= "printk",
 		.data		= &console_loglevel,
 		.maxlen		= 4*sizeof(int),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec,
+		.proc_handler	= printk_sysctl_deprecated,
 	},
 	{
 		.procname	= "printk_ratelimit",
@@ -75,6 +114,22 @@ static struct ctl_table printk_sysctls[] = {
 		.proc_handler	= proc_dointvec_minmax_sysadmin,
 		.extra1		= SYSCTL_ZERO,
 		.extra2		= SYSCTL_TWO,
+	},
+	{
+		.procname	= "console_loglevel",
+		.data		= &console_loglevel,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= printk_console_loglevel,
+	},
+	{
+		.procname	= "default_message_loglevel",
+		.data		= &default_message_loglevel,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &min_loglevel,
+		.extra2		= &max_loglevel,
 	},
 	{}
 };
