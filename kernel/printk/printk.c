7f95710db5b2508623a5fa11289ddb7b7c041917
Chris Down <chris@chrisdown.name>
2024-10-28 16:46:01 +0000

    printk: Purge default_console_loglevel
    default_console_loglevel (then DEFAULT_LOGLEVEL) was created in Linux
    0.99.13 (September 1993) to power what we now call
    SYSLOG_ACTION_CONSOLE_{ON,OFF}. It was originally hardcoded to 7.
    
    In Linux 2.3.12 (March 1997), Chris Horn made it configurable by sysctl
    through kernel.printk.
    
    Its demise came about in July 2009 in commit 1aaad49e856c ("printk:
    Restore previous console_loglevel when re-enabling logging"), which
    replaces default_console_loglevel with the previously used
    console_loglevel. However, the documentation was never updated, and we
    still kept on telling users that this sets the default value for
    console_loglevel, even though this hasn't been true for over 15 years.
    
    Purge both the documentation and all remaining references to
    default_console_loglevel. It will still be initialised in the sysctl
    array.
    
    Reviewed-by: Petr Mladek <pmladek@suse.com>
    Signed-off-by: Chris Down <chris@chrisdown.name>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -67,7 +67,7 @@ int console_printk[4] = {
 	CONSOLE_LOGLEVEL_DEFAULT,	/* console_loglevel */
 	MESSAGE_LOGLEVEL_DEFAULT,	/* default_message_loglevel */
 	CONSOLE_LOGLEVEL_MIN,		/* minimum_console_loglevel */
-	CONSOLE_LOGLEVEL_DEFAULT,	/* default_console_loglevel */
+	/* legacy default console loglevel, unused */
 };
 EXPORT_SYMBOL_GPL(console_printk);
 
